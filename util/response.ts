bdfcc50a9be75372e8cbb3057977996f9841cd65
Dominik Schenk <domi.schenk@gmail.com>
2021-08-30 22:24:56 +0200

    fix: return empty Json repsons if no update is available

--- util/response.ts
+++ util/response.ts
@@ -4,15 +4,22 @@ export function notFound(): Response {
     return new Response("Not found", { status: Status.NotFound });
 }
 export function noUpdateAvailable(): Response {
-    return new Response(null, { status: Status.NoContent });
+    return new Response("{}",
+        {
+            status: Status.NoContent,
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            }
+        });
 }
 
 export function json(obj: unknown, init?: ResponseInit): Response {
-    return new Response(JSON.stringify(obj), {
-        status: init?.status ?? Status.OK,
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            ...init?.headers,
-        },
-    });
+    return new Response(JSON.stringify(obj),
+        {
+            status: init?.status ?? Status.OK,
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                ...init?.headers,
+            }
+        });
 }
